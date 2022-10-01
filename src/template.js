3ed2c9a33592b814a078e740624ceaefeb936170
omkar lolge <omkarlolge119.ol@gmail.com>
2022-10-01 20:49:46 +0530

    no errors

--- src/template.js
+++ src/template.js
@@ -33,7 +33,7 @@ export default function Template(){
                         <button className="btn btn-outline-dark" type="submit">
                             <i className="bi-cart-fill me-1"></i>
                             Cart
-                            <span className="badge bg-dark text-white ms-1 rounded-pill" id="">{carttotal}</span>
+                            <span className="badge bg-dark text-white ms-1 rounded-pill" id="" onClick={setCarttotal(carttotal + 1)}>{carttotal}</span>
                         </button>
                     </form>
                 </div>
